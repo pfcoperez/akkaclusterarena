7e1ec7c056c06799aa860666e51c05ef71d16aff
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-08-10 08:53:16 +0200

    Make server node join to its own cluster programatically

--- server/src/main/scala/com/stratio/clustercanary/EchoServer.scala
+++ server/src/main/scala/com/stratio/clustercanary/EchoServer.scala
@@ -1,12 +1,14 @@
 package com.stratio.clustercanary
 
 import akka.actor.ActorSystem
+import akka.cluster.Cluster
 import akka.cluster.client.ClusterClientReceptionist
 import com.stratio.clustercanary.actors.Echo
 
 object EchoServer extends App {
 
   val system = ActorSystem("CanaryFlock")
+  Cluster(system).join(Cluster(system).selfAddress)
 
   val echoActor = system.actorOf(Echo.props, "echo_cave")
 
